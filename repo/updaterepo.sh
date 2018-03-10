4b73e7b05103fe4335646717606006e517180a4e
eobsite1 <eob site1@gmail.com>
2018-03-10 15:38:08 +0000

    Updated by scrypt

--- repo/updaterepo.sh
+++ repo/updaterepo.sh
@@ -3,3 +3,7 @@
 ./dpkg-scanpackages debs /dev/null > Packages
 gzip -c9 Packages > Packages.gz
 bzip2 -c9 Packages > Packages.bz2
+
+git add -A
+git commit -m "Updated by scrypt"
+git push -u origin master
