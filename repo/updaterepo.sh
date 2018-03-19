20a34950df195d3d53bf4fea9a545f145631473b
eobsite1 <eob site1@gmail.com>
2018-03-19 13:58:07 +0000

    Cleared repo shit

--- repo/updaterepo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-./dpkg-scanpackages debs /dev/null > Packages
-gzip -c9 Packages > Packages.gz
-bzip2 -c9 Packages > Packages.bz2
-
-git add -A
-git commit -m "Updated by scrypt"
-git push -u origin master
